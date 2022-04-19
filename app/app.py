1c6880c7b51bda7a701cd64f241dc13e1e90a2b4
emp7yhead <artyomkropp@gmail.com>
2022-04-19 15:50:42 +0900

    Add: statistics page

--- app/app.py
+++ app/app.py
@@ -89,6 +89,28 @@ def url_redirect(id):  # noqa: WPS125
     return redirect(url_for('index'))
 
 
+@app.route('/stats')
+def stats():
+    """Render statictic page.
+
+    Returns:
+        str.
+    """
+    conn = get_db_connection()
+    db_urls = conn.execute(
+        'SELECT id, created, original_url, clicks FROM urls',
+    ).fetchall()
+    conn.close()
+
+    urls = []
+    for url in db_urls:
+        url = dict(url)
+        url['short_url'] = request.host_url + hashids.encode(url['id'])
+        urls.append(url)
+
+    return render_template('stats.html', urls=urls)
+
+
 @app.route('/about')
 def about():
     """Render about page.
