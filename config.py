6320fcfa42ab7967f928cc431b611cfda87ebeb5
emp7yhead <artyomkropp@gmail.com>
2022-04-27 18:02:56 +0900

    Add: config file

---
+++ config.py
@@ -0,0 +1,15 @@
+"""Config file for app."""
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    """Config variables."""
+
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'urls.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SESSION_PERMANENT = False
+    SESSION_TYPE = 'filesystem'
