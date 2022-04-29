ffa21df8227ecf5a90ac3a027b8c4e108206db7f
emp7yhead <artyomkropp@gmail.com>
2022-04-29 21:26:21 +0900

    Fix: move config.py to app directory

---
+++ app/config.py
@@ -0,0 +1,49 @@
+"""Config file for app."""
+import os
+
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv('.env')
+
+
+class Config(object):
+    """Config variables."""
+
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'very-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+    ) or 'sqlite:///' + os.path.join(  # noqa: WPS336
+        basedir,
+        'urls.db',
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    """Config variables for production."""
+
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    """Config variables for staging."""
+
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    """Config variables for development."""
+
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """Config variables for testing."""
+
+    TESTING = True
